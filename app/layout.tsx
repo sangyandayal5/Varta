129d010fda000a90fb7af4cce39b49ca7ae414f1
Sangyan Dayal <sangyandayal5@gmail.com>
2025-04-14 19:59:55 +0530

    removed nex-superjson-plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,41 +1,41 @@
-import ToasterContext from "./context/ToasterContext";
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import AuthContext from "./context/AuthContext";
-import ActiveStatus from "./components/ActiveStatus";
+import type { Metadata } from 'next'
+import { Inter, Roboto_Mono } from 'next/font/google'
+import './globals.css'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+import AuthContext from './context/AuthContext'
+import ToasterContext from './context/ToasterContext'
+import ActiveStatus from './components/ActiveStatus'
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// Load Google Fonts
+const inter = Inter({
+  variable: '--font-inter',
+  subsets: ['latin'],
+})
+
+const robotoMono = Roboto_Mono({
+  variable: '--font-roboto-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
-  title: "Varta",
-  description: "Messaging App",
-};
+  title: 'Varta',
+  description: 'Messaging App',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${inter.variable} ${robotoMono.variable} antialiased`}>
         <AuthContext>
-        <ToasterContext/>
-        <ActiveStatus/>
-        {children}
+          <ToasterContext />
+          <ActiveStatus />
+          {children}
         </AuthContext>
       </body>
     </html>
-  );
+  )
 }
